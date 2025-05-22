b7dc71e4c2f998a2fa930217edfd0d108a41d2e3
adnanxkfkfk <adnankhan9sije@gmail.com>
2025-05-23 02:30:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   build: {
-    outDir: 'dist', // Matches your vercel.json
-    emptyOutDir: true,
-  },
-  server: {
-    host: true,
-  },
-  base: '/', // Ensure relative paths work properly
-});
+    outDir: 'dist'
+  }
+})
